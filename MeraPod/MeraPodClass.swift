af6ada4f2deab183c039266a1d19f4a0c040ea5b
Rohan Sharma <rohan.sharma@dotsquares.com>
2024-06-10 16:13:11 +0530

    source code

---
+++ MeraPod/MeraPodClass.swift
@@ -0,0 +1,16 @@
+//
+//  MeraPodClass.swift
+//  MeraPod
+//
+//  Created by admin on 10/06/24.
+//
+
+import Foundation
+
+public class MeraPodClass{
+    
+    public static func sum(a: Double, b: Double) -> Double{
+        let sum = (a + b)
+        return sum
+    }
+}
